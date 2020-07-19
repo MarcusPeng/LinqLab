2dd60157785db3153f58e5881b5240eba4473bdc
Marcus <marcus.peng@titansoft.com.sg>
2020-07-20 02:35:45 +0800

    SkipAndTakeLab

---
+++ LinqLab/SkipAndTakeLab.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using LinqLab.Models;
+
+namespace LinqLab
+{
+    public class SkipAndTakeLab
+    {
+        private readonly IEnumerable<Sample> _source;
+
+        public SkipAndTakeLab(IEnumerable<Sample> source)
+        {
+            _source = source;
+        }
+
+        /// <summary>
+        /// 跳過五筆資料
+        /// </summary>
+        /// <returns></returns>
+        public List<Sample> 跳過五筆資料()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 取得五筆資料
+        /// </summary>
+        /// <returns></returns>
+        public List<Sample> 取得五筆資料()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 跳過五筆取得兩筆
+        /// </summary>
+        /// <returns></returns>
+        public List<Sample> 跳過五筆取得兩筆()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
